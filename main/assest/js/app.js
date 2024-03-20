4b97ea51ce429acf4301332d4b1a993bbbb24d11
rezamansourireza <rema49202@gmail.com>
2024-03-20 08:31:06 +0330

    part #4 navbar & sidenav

---
+++ main/assest/js/app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {
+        edge: "right" ,
+        onOpenEnd: () => {
+            alert('hellow baby')
+        }
+    });
+});
