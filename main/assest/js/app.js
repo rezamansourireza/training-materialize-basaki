046c4c49efc21a21ea70a76e556cb2e085f828b9
rezamansourireza <rema49202@gmail.com>
2024-03-24 00:36:04 +0330

    paer #10 svg AccessBar animate

--- main/assest/js/app.js
+++ main/assest/js/app.js
@@ -12,16 +12,41 @@ document.addEventListener('DOMContentLoaded', function () {
         toolbarEnabled: true
     });
 
-    
+
 
     var quickAccessNodes = Array.prototype.slice.call(document.querySelectorAll('.quick_access_items'));
     for (const item of quickAccessNodes) {
         item.addEventListener('click', function () {
             let index = quickAccessNodes.indexOf(item) + 1;
             let menuItemArray = document.querySelectorAll('.menu_item_list');
-            setTimeout(function(){
+            setTimeout(function () {
                 menuItemArray[index].click();
-            } , 100)
+            }, 100)
         })
     }
+
+    animateprogress('pay_progress_path', 'pay_progress_text', 50)
+    animateprogress('floor_progress_path', 'floor_progress_text', 90)
+    function animateprogress(PathDomId, TextDomId, percent) {
+        let startpercent = 0;
+        let maxpercent = percent;
+
+        let stratprogress = 0;
+        let maxprogress = 253;
+
+        let progressInterval = setInterval(function () {
+            if (startpercent <= maxpercent) {
+                document.getElementById(PathDomId).setAttribute('stroke-dasharray', `${stratprogress}, ${maxprogress}`)
+                document.getElementById(TextDomId).innerHTML = startpercent + "%";
+
+                startpercent += 1
+                stratprogress += 2.53
+                maxprogress -= 2.53
+            }else{
+                clearInterval(progressInterval);
+            }
+        }, 25)
+
+    }
+
 });
