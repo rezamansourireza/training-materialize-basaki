68bf7d03cb7a3c748a233fda9c59735d537755db
rezamansourireza <rema49202@gmail.com>
2024-03-23 18:14:12 +0330

    part #8 java

--- main/assest/js/app.js
+++ main/assest/js/app.js
@@ -1,14 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {
-        edge: "right" ,
+        edge: "right",
     });
     var tabs = document.querySelectorAll('.main_ul_navbar');
     M.Tabs.init(tabs, {
-        swipeable:true
+        swipeable: true
     });
     var actionbutton = document.querySelectorAll('.fixed-action-btn');
     M.FloatingActionButton.init(actionbutton, {
         toolbarEnabled: true
     });
+
+    
+
+    var quickAccessNodes = Array.prototype.slice.call(document.querySelectorAll('.quick_access_items'));
+    for (const item of quickAccessNodes) {
+        item.addEventListener('click', function () {
+            let index = quickAccessNodes.indexOf(item) + 1;
+            let menuItemArray = document.querySelectorAll('.menu_item_list');
+            setTimeout(function(){
+                menuItemArray[index].click();
+            } , 100)
+        })
+    }
 });
